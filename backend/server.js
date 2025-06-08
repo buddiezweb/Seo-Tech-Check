964c8bb0b5be21c500bcc1045f4673faaec99398
rajhussainkanani <rajhussainkanani786@gmail.com>
2025-06-08 19:06:39 +0530

    1.1.0015.2

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ mongoose.set('debug', true);
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://seo-tech-check-01-x44ae.ondigitalocean.app/'
+  'https://seo-tech-check-01-x44ae.ondigitalocean.app'
 ];
 
 // CORS Configuration - Allow only specific origins with credentials
